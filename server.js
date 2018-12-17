beb7c17b04c248a4104eb142e382b1b78dbb32ca
SylvainJunca <sylvain.junca@gmail.com>
2018-12-17 14:32:11 -0500

    creates the class object to save and keep tracks of the clients server side

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const SocketServer = require('ws').Server;
 const express = require('express');
+const clients = new Clients();
 
 const PORT = 3001;
 
@@ -7,36 +8,55 @@ const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({
+  server
+});
+
+// This class helps us to keep track of the clients connected
+class Clients {
+  constructor() {
+    this.clientList = {};
+    this.saveClient = this.saveClient.bind(this);
+  }
+  saveClient(username, client) {
+    this.clientList[username] = client;
+  }
+}
 
 const game = {
   gameStage: "welcomeStage",
-  players: ["Valeria","Sylvain","Chris","Alisa"],
+  players: ["Valeria", "Sylvain", "Chris", "Alisa"],
   currentPlayer: "",
   turns: ["Valeria"]
 }
 
 function takeTurns() {
-  if (game.players.length === game.turns.length){
-    game.currentPlayer = "gameOver"; 
+  if (game.players.length === game.turns.length) {
+    game.currentPlayer = "gameOver";
   } else {
-    const playersWhoHaveNotGone = game.players.filter(function(obj) { return game.turns.indexOf(obj) === -1; });
-    const currentPlayer = playersWhoHaveNotGone[Math.floor(Math.random()*playersWhoHaveNotGone.length)];
+    const playersWhoHaveNotGone = game.players.filter(function (obj) {
+      return game.turns.indexOf(obj) === -1;
+    });
+    const currentPlayer = playersWhoHaveNotGone[Math.floor(Math.random() * playersWhoHaveNotGone.length)];
     game.turns.push(currentPlayer);
     game.currentPlayer = currentPlayer;
   }
 }
 
 wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(ws) {
-        ws.send(data);
-    });
-  };
+  wss.clients.forEach(function each(ws) {
+    ws.send(data);
+  });
+};
 
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  
+  ws.on('message', (msg) => {
+    const parsedMsg = Json.parse(msg);
+
+  });
+
   const gameStage = {
     type: "gameStage",
     stage: game.gameStage
@@ -45,22 +65,23 @@ wss.on('connection', (ws) => {
 
   ws.on('message', function (event) {
     let data = JSON.parse(event);
+    if (data.type === 'setupName') {
+      clients.saveClient(data.username, client);
+    }
     if (data.type === "gameStage") {
       game.gameStage = data.stage;
-      wss.broadcast(event);  
-    } else if (data.type === "turns"){
-        takeTurns();
-        const turns = {
-          type: "turns",
-          currentPlayer: game.currentPlayer
-        };
-        wss.broadcast(JSON.stringify(turns));  
+      wss.broadcast(event);
+    } else if (data.type === "turns") {
+      takeTurns();
+      const turns = {
+        type: "turns",
+        currentPlayer: game.currentPlayer
+      };
+      wss.broadcast(JSON.stringify(turns));
     }
   })
 
   ws.on('close', function () {
-      console.log('Client disconnected');
+    console.log('Client disconnected');
   });
 });
-
-
--- src/App.js
+++ src/App.js
@@ -1,145 +1,122 @@
-import React, {
-  Component,
-  Fragment
-} from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-import {
-  BrowserView,
-  MobileView
-} from "react-device-detect";
+import { BrowserView, MobileView } from 'react-device-detect';
 import MobileMainView from './mobileMainView';
 import DesktopMainView from './desktopMainView';
 
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      gameStage: "welcomeStage",
-      mainPlayer: "",
-      players: [{
-        name: "",
-        points: 0
-      }],
-      currentPlayer: "",
-      guessChoices: []
-    };
-    this.changeGameStage = this.changeGameStage.bind(this);
-    this.takeTurns = this.takeTurns.bind(this);
-    this.socket = undefined;
-  }
+	constructor() {
+		super();
+		this.state = {
+			gameStage: 'welcomeStage',
+			mainPlayer: '',
+			players: [
+				{
+					name: '',
+					points: 0
+				}
+			],
+			currentPlayer: '',
+			guessChoices: []
+		};
+		this.changeGameStage = this.changeGameStage.bind(this);
+		this.takeTurns = this.takeTurns.bind(this);
+		this.socket = undefined;
+	}
 
-  //find out what is a static function
-  static getHostName() {
-    const parser = document.createElement('a')
-    parser.href = document.location;
-    return parser.hostname;
-  }
+	//find out what is a static function
+	static getHostName() {
+		const parser = document.createElement('a');
+		parser.href = document.location;
+		return parser.hostname;
+	}
 
-  componentDidMount() {
-    const hostname = App.getHostName();
-    const port = 3001;
-    this.socket = new WebSocket("ws://" + hostname + ":" + port);
-    this.socket.onopen = function (event) {
-      console.log('Connected to: ' + event.currentTarget.url);
-    };
-    this.socket.onmessage = event => {
-      const message = JSON.parse(event.data);
-      if (message.type === "gameStage") {
-        this.setState({
-          gameStage: message.stage
-        })
-      } else if (message.type === "turns") {
-        this.setState({
-          currentPlayer: message.currentPlayer
-        })
-      }
-    }
-  }
+	componentDidMount() {
+		const hostname = App.getHostName();
+		const port = 3001;
+		this.socket = new WebSocket('ws://' + hostname + ':' + port);
+		this.socket.onopen = function(event) {
+			console.log('Connected to: ' + event.currentTarget.url);
+		};
+		this.socket.onmessage = (event) => {
+			const message = JSON.parse(event.data);
+			if (message.type === 'gameStage') {
+				this.setState({
+					gameStage: message.stage
+				});
+			} else if (message.type === 'turns') {
+				this.setState({
+					currentPlayer: message.currentPlayer
+				});
+			}
+		};
+	}
 
-  takeTurns() {
-    //probably won't need this method, kept it here for now for tests
-    const test = {
-      type: "turns"
-    };
-    this.socket.send(JSON.stringify(test));
-  }
+	takeTurns() {
+		//probably won't need this method, kept it here for now for tests
+		const test = {
+			type: 'turns'
+		};
+		this.socket.send(JSON.stringify(test));
+	}
 
-  changeGameStage(stage) {
-    const gameStage = {
-      type: "gameStage",
-      stage
-    };
-    this.socket.send(JSON.stringify(gameStage));
-    this.setState({
-      gameStage: stage
-    })
-  }
+	changeGameStage(stage) {
+		const gameStage = {
+			type: 'gameStage',
+			stage
+		};
+		this.socket.send(JSON.stringify(gameStage));
+		this.setState({
+			gameStage: stage
+		});
+	}
 
-  addPlayerName = name => {
-    console.log(this.state.players);
-    const previousPlayers = this.state.players;
-    const updatePlayers = [...previousPlayers, {
-      name: name,
-      points: 0
-    }];
-    console.log(updatePlayers);
-    this.setState({
-      players: updatePlayers
-    });
-  }
-  // DID THIS UNDER ASSUMPTION THAT PLAYER HAS ONE GUESS AND
-  // THEIR GUESS ARE NOT ASSOCIATED WITH THEIR NAME.. basically the vote is what gets the score? IDK
-  addGuess = guess => {
-    const previousGuess = this.state.guessChoices;
-    const updateGuess = [...previousGuess, guess];
-    this.setState({
-      guessChoices: updateGuess
-    });
-  }
+	addPlayerName = (name) => {
+		console.log(name);
+		this.setState({
+			mainPlayer: name
+		});
+		const setName = {
+			type = 'setName',
+			username = name
+		};
+		this.socket.send(JSON.stringify(setName));
+	};
+	// DID THIS UNDER ASSUMPTION THAT PLAYER HAS ONE GUESS AND
+	// THEIR GUESS ARE NOT ASSOCIATED WITH THEIR NAME.. basically the vote is what gets the score? IDK
+	addGuess = (guess) => {
+		const previousGuess = this.state.guessChoices;
+		const updateGuess = [ ...previousGuess, guess ];
+		this.setState({
+			guessChoices: updateGuess
+		});
+	};
 
-
-  render() {
-    return ( <
-      Fragment >
-      <
-      h3 style = {
-        {
-          textAlign: 'center'
-        }
-      } > Draw Daddy < /h3> <
-      button onClick = {
-        this.takeTurns
-      } > take turns < /button> <
-      BrowserView >
-      <
-      DesktopMainView stage = {
-        this.state
-      }
-      changeGameStage = {
-        this.changeGameStage
-      }
-      /> <
-      /BrowserView> <
-      MobileView >
-      <
-      MobileMainView stage = {
-        this.state
-      }
-      addPlayerName = {
-        this.addPlayerName
-      }
-      addGuess = {
-        this.addGuess
-      }
-      changeGameStage = {
-        this.changeGameStage
-      }
-      /> <
-      /MobileView> <
-      /Fragment>
-    );
-  }
+	render() {
+		return (
+			<Fragment>
+				<h3
+					style={{
+						textAlign: 'center'
+					}}
+				>
+					Draw Daddy
+				</h3>
+				<button onClick={this.takeTurns}> take turns </button>
+				<BrowserView>
+					<DesktopMainView stage={this.state} changeGameStage={this.changeGameStage} />
+				</BrowserView>
+				<MobileView>
+					<MobileMainView
+						stage={this.state}
+						addPlayerName={this.addPlayerName}
+						addGuess={this.addGuess}
+						changeGameStage={this.changeGameStage}
+					/>
+				</MobileView>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
