4c2bcf9d1ad661a40419bcea72d1f4db33fdf1ee
SylvainJunca <sylvain.junca@gmail.com>
2018-12-17 09:02:53 -0500

    refactors the conditional rendering

--- src/desktopMainView.js
+++ src/desktopMainView.js
@@ -1,55 +1,39 @@
-import React, {Component} from 'react';
-import DesktopWelcome from './desktopWelcomeScreen'
-import Timer from './desktopTimerFooter'
-import DesktopDrawingScreen from './desktopDrawingScreen'
-import DesktopGuessingScreen from './desktopGuessingScreen'
-import DesktopVotingScreen from './desktopVotes'
-import DesktopScoresScreen from './desktopScores'
+import React, { Component } from 'react';
+import DesktopWelcome from './desktopWelcomeScreen';
+import Timer from './desktopTimerFooter';
+import DesktopDrawingScreen from './desktopDrawingScreen';
+import DesktopGuessingScreen from './desktopGuessingScreen';
+import DesktopVotingScreen from './desktopVotes';
+import DesktopScoresScreen from './desktopScores';
 
 class DesktopMainView extends Component {
-  render() {
-    if (this.props.stage.gameStage === "welcomeStage") {
-      return (
-        <div>
-          <p>Hello desktop user</p>
-          <p>Current stage: {this.props.stage.gameStage} </p>
-          <DesktopWelcome changeGameStage={this.props.changeGameStage}/>
-          {/* <Timer /> */}
-        </div>
-      );
-    } else if (this.props.stage.gameStage === "drawingStage") {
-      return (
-        <div>
-          <p>Hello Desktop User</p>
-          <p>Current stage: {this.props.stage.gameStage} </p>
-          <DesktopDrawingScreen changeGameStage={this.props.changeGameStage} />
-        </div>
-      );
-    } else if (this.props.stage.gameStage === "guessingStage") {
-      return (
-        <div>
-          <p>Hello Desktop User</p>
-          <p>Current Stage: {this.props.stage.gameStage} </p>
-          <DesktopGuessingScreen changeGameStage={this.props.changeGameStage} />
-        </div>
-      );
-    } else if (this.props.stage.gameStage === "votingStage") {
-      return (
-        <div>
-          <p>Hello Desktop User</p>
-          <p>Current Stage: {this.props.stage.gameStage} </p>
-          <DesktopVotingScreen changeGameStage={this.props.changeGameStage} />
-        </div>
-      );
-    } else if (this.props.stage.gameStage === "scoreStage") {
-      return (
-        <div>
-          <p>Hello Desktop User</p>
-          <p>Current Stage: {this.props.stage.gameStage} </p>
-          <DesktopScoresScreen changeGameStage={this.props.changeGameStage} />
-        </div>
-      );
-    }
-  }
+	render() {
+		let view;
+		switch (this.props.stage.gameStage) {
+			case 'welcomeStage':
+				view = <DesktopWelcome changeGameStage={this.props.changeGameStage} />;
+				break;
+			case 'drawingStage':
+				view = <DesktopDrawingScreen changeGameStage={this.props.changeGameStage} />;
+				break;
+			case 'guessingStage':
+				view = <DesktopGuessingScreen changeGameStage={this.props.changeGameStage} />;
+				break;
+			case 'votingStage':
+				view = <DesktopVotingScreen changeGameStage={this.props.changeGameStage} />;
+				break;
+			case 'scoreStage':
+				view = <DesktopScoresScreen changeGameStage={this.props.changeGameStage} />;
+				break;
+		}
+		return (
+			<div>
+				<p>Hello desktop user</p>
+				<p>Current stage: {this.props.stage.gameStage} </p>
+				{view}
+				{/* <Timer /> */}
+			</div>
+		);
+	}
 }
 export default DesktopMainView;
