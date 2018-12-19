a518bf63101a51d21ed102fe3900520353df61b0
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-12-19 01:25:59 +0000

    Adding score logic to button

--- src/desktopTimerFooter.js
+++ src/desktopTimerFooter.js
@@ -5,22 +5,22 @@ class Timer extends Component {
 
 
   render() {
-    // Progress Bar Timer
-    let timeleftBar = 30;
-    var downloadTimerBar = setInterval(function () {
-      document.getElementById("progressBar").value = 30 - --timeleftBar;
-      if (timeleftBar <= 0)
-        clearInterval(downloadTimerBar);
-    }, 1000);
+    // // Progress Bar Timer
+    // let timeleftBar = 30;
+    // var downloadTimerBar = setInterval(function () {
+    //   document.getElementById("progressBar").value = 30 - --timeleftBar;
+    //   if (timeleftBar <= 0)
+    //     clearInterval(downloadTimerBar);
+    // }, 1000);
 
-    // Countdown Timer
-    let timeleftCounter = 30;
-    let downloadTimerCounter = setInterval(function(){
-    timeleftCounter--;
-    document.getElementById("countTimer").textContent = timeleftCounter;
-    if(timeleftCounter <= 0)
-        clearInterval(downloadTimerCounter);
-    },1000);
+    // // Countdown Timer
+    // let timeleftCounter = 30;
+    // let downloadTimerCounter = setInterval(function(){
+    // timeleftCounter--;
+    // document.getElementById("countTimer").textContent = timeleftCounter;
+    // if(timeleftCounter <= 0)
+    //     clearInterval(downloadTimerCounter);
+    // },1000);
 
     return (
       <div className="desktopTimerContainer">
--- src/mobileVotes.js
+++ src/mobileVotes.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
 class Votes extends Component {
+
+  handleEvent = (event) => {
+    const guess = event.target.value;
+    const mainPlayer = this.props.gameData.mainPlayer;
+    const currentPlayer = this.props.gameData.currentPlayer;
+    const currentPlayerTask = this.props.gameData.playerGuess[currentPlayer];
+    
+    // this.props.changeGameStage("guessingStage");
+  }
+
+
   render() {
     return (
       <div>
-        <button>{this.props.guess}</button>
+        <button onTouchStart={this.handleEvent} value={this.props.guess}>{this.props.guess}</button>
       </div>
     );
   }
