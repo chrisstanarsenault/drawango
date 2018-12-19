0af721a187cc47f35986f484d2f49da9ceffb551
SylvainJunca <sylvain.junca@gmail.com>
2018-12-18 19:31:13 -0500

    creates the timer function server side

--- server.js
+++ server.js
@@ -2,102 +2,113 @@ const SocketServer = require('ws').Server;
 const express = require('express');
 const PORT = 3001;
 const server = express()
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+	.use(express.static('public'))
+	.listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 const wss = new SocketServer({
-  server
+	server
 });
 
 const game = {
-  gameStage: "welcomeStage",
-  players: [],
-  currentPlayer: "",
-  turns: [],
-  playerGuess: {},
-  line: []
-}
+	gameStage: 'welcomeStage',
+	players: [],
+	currentPlayer: '',
+	turns: [],
+	playerGuess: {},
+	line: []
+};
 
-const draw = "Cat"
+const draw = 'Cat';
+
+timer = (time, stage) => {
+	let timeleftCounter = time;
+	let downloadTimerCounter = setInterval(function() {
+		timeleftCounter--;
+		if (timeleftCounter <= 0) {
+			clearInterval(downloadTimerCounter);
+			wss.broadcast(JSON.stringify({ type: 'gameStage', stage: stage }));
+		}
+	}, 1000);
+};
 
 function takeTurns() {
-  if (game.players.length === game.turns.length) {
-    game.currentPlayer = "gameOver";
-  } else {
-    const playersWhoHaveNotGone = game.players.filter(function (obj) {
-      return game.turns.indexOf(obj) === -1;
-    });
-    const currentPlayer = playersWhoHaveNotGone[Math.floor(Math.random() * playersWhoHaveNotGone.length)];
-    game.turns.push(currentPlayer);
-    game.currentPlayer = currentPlayer;
-  }
+	if (game.players.length === game.turns.length) {
+		game.currentPlayer = 'gameOver';
+	} else {
+		const playersWhoHaveNotGone = game.players.filter(function(obj) {
+			return game.turns.indexOf(obj) === -1;
+		});
+		const currentPlayer = playersWhoHaveNotGone[Math.floor(Math.random() * playersWhoHaveNotGone.length)];
+		game.turns.push(currentPlayer);
+		game.currentPlayer = currentPlayer;
+	}
 }
 
 wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(ws) {
-    ws.send(data);
-  });
+	wss.clients.forEach(function each(ws) {
+		ws.send(data);
+	});
 };
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+	console.log('Client connected');
 
-  const welcomePack = {
-    type: "welcomePack",
-    gameStage: game.gameStage,
-    players: game.players,
-    currentPlayer: game.currentPlayer,
-    playerGuess: game.playerGuess,
-    line: game.line
-  };
-  ws.send(JSON.stringify(welcomePack));
+	const welcomePack = {
+		type: 'welcomePack',
+		gameStage: game.gameStage,
+		players: game.players,
+		currentPlayer: game.currentPlayer,
+		playerGuess: game.playerGuess,
+		line: game.line
+	};
+	ws.send(JSON.stringify(welcomePack));
 
-  ws.on('message', function (event) {
-    let data = JSON.parse(event);
-    switch (data.type) {
-      case 'setName':
-        const addPlayer = { 
-          name: data.player, 
-          points: 0,
-          task: draw
-        }
-        game.players.push(addPlayer);
-        const players = {
-          type: "addPlayer",
-          players: game.players
-        }
-        wss.broadcast(JSON.stringify(players));
-        break;
-      case 'turns':
-        takeTurns();
-        const turns = {
-          type: "turns",
-          currentPlayer: game.currentPlayer,
-        };
-        wss.broadcast(JSON.stringify(turns));
-        break
-      case 'canvas':
-        game.line = data.line;
-        wss.broadcast(event);
-        break
-      case 'setGuess':
-        game.playerGuess[data.player] = data.guess;
-        game.playerGuess[game.currentPlayer.name] = draw;
-        const guesses = {
-          type: "addGuess",
-          guesses: game.playerGuess
-        };
-        wss.broadcast(JSON.stringify(guesses));
-        break;
-      case "gameStage":
-        game.gameStage = data.stage;
-        wss.broadcast(event);
-        break;
-      default:
-			  throw new Error("Unknown event type " + data.type);
-    }
-  })
+	ws.on('message', function(event) {
+		let data = JSON.parse(event);
+		switch (data.type) {
+			case 'setName':
+				const addPlayer = {
+					name: data.player,
+					points: 0,
+					task: draw
+				};
+				game.players.push(addPlayer);
+				const players = {
+					type: 'addPlayer',
+					players: game.players
+				};
+				wss.broadcast(JSON.stringify(players));
+				break;
+			case 'turns':
+				takeTurns();
+				const turns = {
+					type: 'turns',
+					currentPlayer: game.currentPlayer
+				};
+				wss.broadcast(JSON.stringify(turns));
+				break;
+			case 'canvas':
+				game.line = data.line;
+				wss.broadcast(event);
+				break;
+			case 'setGuess':
+				game.playerGuess[data.player] = data.guess;
+				game.playerGuess[game.currentPlayer.name] = draw;
+				const guesses = {
+					type: 'addGuess',
+					guesses: game.playerGuess
+				};
+				wss.broadcast(JSON.stringify(guesses));
+				break;
+			case 'gameStage':
+				game.gameStage = data.stage;
+				wss.broadcast(event);
+				break;
+			default:
+				throw new Error('Unknown event type ' + data.type);
+		}
+	});
 
-  ws.on('close', function () {
-    console.log('Client disconnected');
-  });
+	ws.on('close', function() {
+		console.log('Client disconnected');
+	});
 });
