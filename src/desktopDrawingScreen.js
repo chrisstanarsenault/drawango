736fc7df036115efd4ec9db0a100abb574c898f2
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-12-20 20:48:45 +0000

    pages refresh based on the timer

--- src/desktopDrawingScreen.js
+++ src/desktopDrawingScreen.js
@@ -6,8 +6,9 @@ class DesktopDrawingScreen extends Component {
 	render() {
 		return (
 			<div>
-				<p> {this.props.gameData.currentPlayer} is drawing </p>
-				<Canvas gameData={this.props.gameData} />
+				<p>{this.props.gameData.currentPlayer} is drawing</p>
+				<Canvas gameData={this.props.gameData}/>
+				<Timer gameData={this.props.gameData} changeGameStage={this.props.changeGameStage} stage={"guessingStage"} resetTimer={this.props.resetTimer}/>
 			</div>
 		);
 	}
--- src/desktopGuessingScreen.js
+++ src/desktopGuessingScreen.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
 import Canvas from './canvas'
+import Timer from './desktopTimerFooter';
 
 class DesktopGuessingScreen extends Component {
-
-  handleEvent = event => {
-    this.props.changeGameStage("votingStage");
-  }
 
   render() {
     return (
       <div>
-        <p> What do you think this is? Submit your guesses </p>
-        <button onClick={this.handleEvent}> Test Next Stage - this will be removed </button>
+        <p> What do you think this is? Submit your guess</p>
         <Canvas gameData={this.props.gameData} />
+        <Timer gameData={this.props.gameData} changeGameStage={this.props.changeGameStage} resetTimer={this.props.resetTimer} stage={"votingStage"}/>
       </div>
     )
   }
