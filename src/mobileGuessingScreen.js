342dacc6cf01e43104b50c3584aab6657468dd72
SylvainJunca <sylvain.junca@gmail.com>
2018-12-20 13:49:45 -0500

    adds the logic to validate a guess

--- src/mobileGuessingScreen.js
+++ src/mobileGuessingScreen.js
@@ -2,11 +2,44 @@ import React, {Component} from 'react';
 
 class MobileGuessingScreen extends Component {
 
+  // validateAnswer = answer => {
+  //   // trimAnswer = answer.replace(/^\s+|\s+$/g, "");
+  //   let check = true;
+  //   for (const each in this.props.gameData.playerGuess) {
+  //     if (answer === this.props.gameData.playerGuess[each])
+  //     console.log(this.props.gameData.playerGuess[each]);
+  //     check = false;
+  //   }
+  //   return check;
+  // }
+
   handleSubmit = event => {
     event.preventDefault();
     const guessInput = event.target.elements.guess;
-    this.props.addGuess(guessInput.value);
+    const guesses = this.props.gameData.playerGuess;
+    let check = true
+    for (const each in guesses) {
+      console.log(guesses[each])
+      if (guessInput.value === guesses[each]) {
+      alert('You should try another guess');
+      check = false
+      }
     }
+
+
+  //  for (let i=0; i < this.props.gameData.players.length; i++)  {
+  //    if (this.props.gameData.playerGuess[this.props.gameData.players[i].name]) {
+  //     this.props.gameData.playerGuess[this.props.gameData.players[i]] === guessInput
+  //    }
+  //  }
+
+
+    if (check) {
+      this.props.addGuess(guessInput.value)
+    }
+  }
+
+  
 
   render() {
 
@@ -31,6 +64,7 @@ class MobileGuessingScreen extends Component {
           <input type="submit" value="Guess" />
         </form>
         <p>{guess}</p>
+       
       </div>
     );
   }
