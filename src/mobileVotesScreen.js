3301163f8bfc7cba6519d1833f1bdc747221059c
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-12-20 02:07:38 +0000

    fixed the bugs

--- src/mobileVotesScreen.js
+++ src/mobileVotesScreen.js
@@ -3,15 +3,13 @@ import Votes from './mobileVotes';
 
 class MobileVotesScreen extends Component {
   
-
-
-
   render() {
 
     const players = [];
+    console.log("AAAAAAAA",this.props.gameData.playerGuess)
+
     this.props.gameData.players.forEach(player => { 
       if (this.props.gameData.playerGuess[player.name] && player.name !== this.props.gameData.mainPlayer) {
-        if (player.name !== this.props.gameData.mainPlayer) {}
         players.push(player);
       }
     });
@@ -19,10 +17,19 @@ class MobileVotesScreen extends Component {
     const guesses = players.map(player => (
       <Votes key={player.name} player={player.name} guess={this.props.gameData.playerGuess[player.name]} gameData={this.props.gameData} addPoints={this.props.addPoints}/>
       ));
-    
+
+      console.log("this is the vote",this.props.gameData.playerVote[this.props.gameData.mainPlayer])
+
+    if (!this.props.gameData.playerVote[this.props.gameData.mainPlayer]) {
+      return (
+        <div>
+          {guesses}
+        </div>
+      );
+    }
     return (
       <div>
-        {guesses}
+        <p>This is the default page</p>
       </div>
     );
   }
