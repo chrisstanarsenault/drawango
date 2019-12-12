423be3a6177f59eed639d516fef05fd6647197aa
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-12-12 01:46:20 -0500

    tried added a secure connect to the websocket

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 	componentDidMount() {
 		const hostname = App.getHostName();
 		const port = 3001;
-		this.socket = new WebSocket('ws://' + hostname + ':' + port);
+		this.socket = new WebSocket('wss://' + hostname + ':' + port);
 		this.socket.onopen = function (event) {
 			console.log('Connected to: ' + event.currentTarget.url);
 		};
